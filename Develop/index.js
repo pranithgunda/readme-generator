6871e85bdd5cf9e63dd3bb2fd2f511a00e601de0
Pranith Gunda <pranith0527@yahoo.com>
2024-06-11 12:36:13 -0400

    functions to generate license link, badge and section are completed successfully. Defined init function to import required packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,87 +1,88 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const markDown = require('./utils/generateMarkdown.js');
-const fs = require('fs');
+// Initialize markdown and fs variables
+let markDown = "";
+let fs = "";
 // Initiate licenses array for license selection
 const licenses = ['Apache 2.0',
-'Boost Software License 1.0',
-'BSD 3-Clause License',
-'Eclipse Public License 1.0',
-'GNU GPL v3',
-'The MIT License',
-'Mozilla Public License 2.0',
-'The Perl License']
-// TODO: Create an array of questions for user input
-const questions =[
-"Project Title?", 
-"Description?",
-"Installation Instructions?",
-"Usage Information",
-"Contribution Guidelines",
-"Test Instructions",
-"License"
+    'Boost Software License 1.0',
+    'BSD 3-Clause License',
+    'Eclipse Public License 1.0',
+    'GNU GPL v3',
+    'The MIT License',
+    'Mozilla Public License 2.0',
+    'The Perl License']
+// Array to prompt user with questions
+const questions = [
+    "Project Title?",
+    "Description?",
+    "Installation Instructions?",
+    "Usage Information",
+    "Contribution Guidelines",
+    "Test Instructions",
+    "License"
 ];
 
 inquirer
     .prompt([
-    {
-        type: "input",
-        message: questions[0],
-        name: "title"
+        {
+            type: "input",
+            message: questions[0],
+            name: "title"
 
-    },
-    {
-        type:"input",
-        message:questions[1],
-        name:"description"
-    },
-    {
-        type:"input",
-        message:questions[2],
-        name:"installation_instructions"
-    },
-    {
-        type:"input",
-        message:questions[3],
-        name:"usage_information"
-    },
-    {
-        type:"input",
-        message:questions[4],
-        name:"contribution_guidelines"
-    },
-    {
-        type:"input",
-        message:questions[5],
-        name:"test_instructions"
-    },
-    {
-        type:"list",
-        message:questions[6],
-        name:"license",
-        choices:licenses
-    }
-])
-        .then((response) => {
-            return response
-        })
-        .then((data)=>{
-           const markDownData = markDown.generateMarkdown(data);
-           writeToFile("README.md",markDownData);
-        })
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "description"
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "installation_instructions"
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "usage_information"
+        },
+        {
+            type: "input",
+            message: questions[4],
+            name: "contribution_guidelines"
+        },
+        {
+            type: "input",
+            message: questions[5],
+            name: "test_instructions"
+        },
+        {
+            type: "list",
+            message: questions[6],
+            name: "license",
+            choices: licenses
+        }
+    ])
+    .then((response) => {
+        return response
+    })
+    .then((data) => {
+        const markDownData = markDown.generateMarkdown(data);
+        writeToFile("README.md", markDownData);
+    })
 
 
 
-// TODO: Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName,data,(err)=>
+    fs.writeFile(fileName, data, (err) =>
         err ? console.error(err) : console.log('Success!'))
- };
+};
 
-// TODO: Create a function to initialize app
-function init() { 
-
+// function to initialize app
+function init() {
+    markDown = require('./utils/generateMarkdown.js');
+    fs = require('fs');
 }
 
-// Function call to initialize app
+// Function call to initialize app by importing required packages
 init();
